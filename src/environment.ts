705b3049c33978088adf77a57f38b33323e2c192
David Nguyen <deepfriedcoconut@gmail.com>
2018-08-10 02:28:38 +1000

    Configured base code

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBGrA6mA6AhYFAoN2CRJLThK4Tzza1YJNg",
+    authDomain: "rosterstaff-d7906.firebaseapp.com",
+    databaseURL: "https://rosterstaff-d7906.firebaseio.com",
+    projectId: "rosterstaff-d7906",
+    storageBucket: "rosterstaff-d7906.appspot.com",
+    messagingSenderId: "747101648316"
+};
